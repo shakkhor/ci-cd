fd752569ad048b568af4846083cecf9becea4885
abdullah-shakkhor <121848754+abdullah-shakkhor@users.noreply.github.com>
2023-09-04 16:37:12 +0600

    second web project added.

---
+++ webApp2/src/main/java/com/shakkhor/cicd/CiCdApplication2.java
@@ -0,0 +1,13 @@
+package com.shakkhor.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdApplication2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdApplication2.class, args);
+	}
+
+}
---
+++ webApp2/src/test/java/com/shakkhor/cicd/CiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shakkhor.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
